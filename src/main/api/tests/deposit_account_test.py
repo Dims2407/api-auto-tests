56403ff7813eeddbfd83702bad2604bbb7684f87
Dims2407 <108788284+Dims2407@users.noreply.github.com>
2026-07-21 17:41:52 +0300

    senior optimization for deposit_account_test.py random amount for valid deposit account

--- src/main/api/tests/deposit_account_test.py
+++ src/main/api/tests/deposit_account_test.py
@@ -1,19 +1,24 @@
 import pytest
 from src.main.api.models.deposit_account_request import DepositAccountRequest
-
+from random import uniform
 
 
 @pytest.mark.api
 class TestDepositAccount:
+
     def test_deposit_account(self, api_manager, create_user_request):
+        amount = round(uniform(1000, 9000), 2)
+
+
         response = api_manager.user_steps.create_account(create_user_request)
         assert response.balance == 0
         account_id = response.id
 
-        deposit_account_request = DepositAccountRequest(accountId=account_id, amount=6666.6)
+        deposit_account_request = DepositAccountRequest(accountId=account_id, amount=amount)
 
         response = api_manager.user_steps.deposit_account(create_user_request, deposit_account_request)
-        assert response.balance == 6666.6
+        assert response.balance == amount
+
 
     @pytest.mark.parametrize(
         "amount",
